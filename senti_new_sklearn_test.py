2735eb1652ae40a9c641bd8215b5e83acdea1baf
Abhishek Tiwari <tiwariabhishekk10@gmail.com>
2020-03-08 18:48:37 +0530

    Update senti_new_sklearn_test.py

--- senti_new_sklearn_test.py
+++ senti_new_sklearn_test.py
@@ -4,15 +4,13 @@ import pickle
 
 from sklearn.feature_extraction.text import CountVectorizer
 
-#new data
-data_new=pd.read_csv(r"D:\sentimental analysis\zp_msg.csv")
-data_new.head()
-data_new=data_new[data_new.columns[6:7]]
-data_new
-data_new1=data_new.dropna(how='all')
-data_new1
-data_new2=data_new1['msg']
-data_new2
+url='https://raw.githubusercontent.com/tiwariabhishekk10/Sentimental-Analysis/master/test.csv'
+
+data=pd.read_csv(url)
+data.head()
+
+data=data.dropna(how='all')
+data
 
 x_test=data['Messages'][int(0.8*(len(data['Messages']))):]
 x_test
@@ -35,16 +33,5 @@ with open ('model_sentimental_analysis_new','rb') as file:
     mp_new=pickle.load(file)
 
 #prediction
-model=mp_new.fit(x_test_cv,y_test)
-predictions=mp_new.predict(predict_data_cv)
-predictions
-
-predictions=pd.DataFrame(predictions,columns=['Prediction'])
-predictions
-
-predictions=predictions.reindex_like(data_new1,'ffill')
-predictions=predictions.rename(columns={'msg':'Predictions'})
-predictions
-
-predict_data=pd.concat([data_new,predictions],axis=1)#axis = 1 for column and axis = 0 for row
-predict_data
+model=mp_new.predict(x_test_cv,y_test)
+model
